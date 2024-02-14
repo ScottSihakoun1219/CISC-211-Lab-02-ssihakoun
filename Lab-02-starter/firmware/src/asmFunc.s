c4f8d392d7e0eacadab74e70a9bd7e717d0c687e
ScottSihakoun1219 <159092423+ScottSihakoun1219@users.noreply.github.com>
2024-02-13 19:59:26 -0800

    Fixed bug in asmFuncs

--- Lab-02-starter/firmware/src/asmFunc.s
+++ Lab-02-starter/firmware/src/asmFunc.s
@@ -54,7 +54,7 @@ asmFunc:
 
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-    add r0,r0,r1
+    ADD r0,r0,r1
 
     
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
